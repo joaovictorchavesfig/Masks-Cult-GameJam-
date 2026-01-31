9c38f95f3e077d382676fa936f9841fba0e2c8fa
Joaovictorchavesfig <joaovictorchavesfig@gmail.com>
2026-01-30 22:45:14 -0300

    mudancas matheus

--- structures/altar.gd
+++ structures/altar.gd
@@ -1,30 +1,47 @@
 extends Area2D
 class_name Altar
 
-var hasLeader: bool = false
 var peopleInAltar: int = 0
-@onready var label: Label = $"../PeopleNumber/Label"
+var leaderInAltar: int = 0
+@onready var label: Label = $"../PeopleNumber/Label" # Verifique se esse caminho está certo na sua cena!
 
-func _init() -> void:
-	print(peopleInAltar)
+func _ready() -> void:
+	# É melhor usar _ready do que _init para manipular UI
+	label.text = str(peopleInAltar + leaderInAltar)
 
 func _on_body_entered(body: Node2D) -> void:
-	body.queue_free()
+	# 1. VERIFICAÇÃO DE SEGURANÇA: É o líder?
 	if body is Leader:
-		hasLeader = true
+		leaderInAltar += 1
+		update_label()
+		return # PARE O CÓDIGO AQUI. Não deixe ele descer para o queue_free()
 	
+	# 2. Se o código chegou aqui, significa que NÃO é o líder.
+	# Então é um civil/seguidor.
+	body.queue_free() # Deleta o boneco da tela
 	peopleInAltar += 1
-	label.text = str(peopleInAltar)
+	update_label()
 
+func _on_body_exited(body: Node2D) -> void:
+	# É importante detectar quando o líder sai andando
+	if body is Leader:
+		leaderInAltar -= 1
+		update_label()
+
+# --- BOTÕES ---
 
 func _on_plus_button_pressed() -> void:
 	if GameManager.idle_people > 0:
 		GameManager.remove_idle_people()
 		peopleInAltar += 1
-		label.text = str(peopleInAltar)
+		update_label()
 
 func _on_minus_button_pressed() -> void:
 	if peopleInAltar > 0:
 		GameManager.add_idle_people()
 		peopleInAltar -= 1
-		label.text = str(peopleInAltar)
+		update_label()
+
+# Função auxiliar para não repetir código
+func update_label() -> void:
+	label.text = str(peopleInAltar + leaderInAltar)
--- structures/ui_mouse_detector.gd
+++ structures/ui_mouse_detector.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@onready var area_2d: Altar = $"../Area2D"
 @onready var container: Node = $"../Container"
+@onready var remove_leader_button: Button = $"../RemoveLeaderButton"
 
 func _on_mouse_entered() -> void:
 	container.show()
